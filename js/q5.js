b6a817abb12fa74780adba4b681cc081a0005f6c
Lucas G. Oliveira <lucassantosdosreis369@gmail.com>
2026-09-22 17:33:02 -0300

    Add files via upload

---
+++ js/q5.js
@@ -0,0 +1,38 @@
+/* 5. Crie uma função chamada somarElementos que receba um array de números
+como parâmetro, percorra o vetor, some todos os valores e retorne o total.
+
+entrada: array e os elementos dentro do array
+processamento: somar os valores dentro do array
+saída: exibir o valor da soma
+
+*/
+
+function receberarray(){
+    const valores = []
+    let tamanho = Number(prompt("Digite quantos valores que deseja colocar:"))
+    for(let i = 0; i < tamanho; i ++){
+        valores[i] = Number(prompt(`Digite o ${i + 1}° valor`))
+    }
+    return valores
+}
+
+function somarElementos(vetor){
+
+    let soma = 0
+
+    for(let elemento of vetor){
+        soma += elemento
+    }   
+    return soma
+}
+
+
+function exibir(resu){
+    alert(`O valor da soma é: ${resu} `)
+}
+
+const array = receberarray()
+
+let resultado = somarElementos(array)
+
+exibir(resultado)
